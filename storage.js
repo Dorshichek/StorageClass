704cc44bb272427c50dbb7d722ee67d356618d24
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-04-18 00:51:47 +0300

    add Storage

---
+++ storage.js
@@ -0,0 +1,40 @@
+class Storage {
+
+  constructor( key, value, storage = localStorage,) {
+    this.storage = storage
+    this.key = key
+    this.value = value
+  }
+
+  get() {
+    return this.storage.getItem(this.key)
+  }
+
+  set() {
+    this.storage.setItem(this.key, this.value)
+  }
+
+  clear() {
+    this.storage.clear()
+  }
+
+  isEmpty() {
+    const key = this.storage.getItem('lel')
+    return (!(key !== null && key !== undefined))
+  }
+}
+
+const local = new Storage('first','value')
+
+const session = new Storage('second','someValue', sessionStorage)
+
+local.set()
+console.log(local.get())
+local.clear()
+console.log(local.isEmpty())
+
+session.set()
+console.log(session.get())
+session.clear()
+console.log(session.isEmpty())
+
